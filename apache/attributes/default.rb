a000ff74af109dcdb386190f3439e0cac67f326e
Root User <anees.shaik9@gmail.com>
2016-02-21 03:58:44 +0000

    FIRST COMMIT

---
+++ apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["anees-shaik9-gmail-com3"] = {"port" => 80, "domain" => "anees-shaik9-gmail-com3.mylabserver.com" }
+default["apache"]["sites"]["anees-shaik9-gmail-com3b"] = {"port" => 80, "domain" => "anees-shaik9-gmail-com3b.mylabserver.com"}
